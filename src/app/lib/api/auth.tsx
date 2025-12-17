473615a9b08ef194b31c879c525a3978b815f6c7
evanrpc <evanrose99@gmail.com>
2025-12-16 20:02:47 -0500

    login

--- src/app/lib/api/auth.tsx
+++ src/app/lib/api/auth.tsx
@@ -1,8 +1,31 @@
 const base_api_url = "http://localhost:5000/";
 
 export async function authenticateUser(email: string, password: string) {
-    await new Promise((resolve) => setTimeout(resolve, 500));
-    return { status: true, token: "fake-jwt-token" };
+    // await new Promise((resolve) => setTimeout(resolve, 500));
+    // return { status: true, token: "fake-jwt-token" };
+    const jsonBody = {
+        email: email,
+        passkey: password
+    };
+
+    const response = await fetch(base_api_url + "api/login", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(jsonBody),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        throw new Error(data.message ?? "Login failed");
+    }
+
+    console.log(data.user + "");
+    console.log("RAW RESPONSE:", data + "");
+    return true;
 }
 
 export async function createUser(email: string, password: string, firstName: string, lastName: string) {
@@ -21,19 +44,19 @@ export async function createUser(email: string, password: string, firstName: str
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(jsonBody),
-    })
+    });
     response.json().then(data => {
         if (data.status == "success") {
             console.log('Success:', data);
-            return { status: true };
+            return true;
 
         } else {
             console.log('Failed:', data);
-            return { status: false };
+            return false;
         }
     })
     .catch((error) => {
         console.error('Error:', error);
-        return { status: false };
+        return false;
     });
 }
